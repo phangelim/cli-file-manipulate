e1fc86635488dfe0144885a52e117c51514cf90f
Pedro Henrique <phlclima2007@gmail.com>
2026-09-05 18:37:40 -0300

    Method loop add

--- src/main.ts
+++ src/main.ts
@@ -1,87 +1,5 @@
 import * as fs from "node:fs";
 
-
-class fileManager {
-  private fileManager: number = 0;
-
-  constructor(fileManager: number) {
-    this.fileManager = fileManager;
-  };
-
-  listPath(path: string) {
-    const cont: string[] = fs.readdirSync(path);
-
-    for (let i = 0; i <= path.length; i++) {
-      let content = cont[i];
-
-      console.log(content)
-    };
-  }
-
-
-  exitCli() { process.exit(0); };
-
-
-  create(num: number) {
-    if (num == 1) {
-      const file: string = prompt("Write the file with the extension:")!;
-
-      try {
-        fs.writeFileSync(`${file}`, " is created with CLI inside this directory!!!");
-      }
-      catch (error) {
-        console.error("Error to create the file");
-      };
-    }
-
-    else if (num == 2) {
-      const nameFolder: string = prompt("Write the name of folder:")!;
-
-      try {
-        fs.mkdirSync(`${nameFolder}`);
-      }
-      catch (error) {
-        console.error(`Error to create the folder ${nameFolder}`);
-      };
-    };
-  }
-
-  delete() {
-    const pathDelete: string = prompt("Write the path to Delete the file:")!;
-
-    try {
-      fs.rmSync(`${pathDelete}`);
-    }
-    catch (error) {
-      console.error(`Error to Delete the file`);
-    };
-  }
-
-  rename() {
-    const renameFile: string = prompt("Enter the path of the file for rename:")!;
-
-    const newName: string = prompt("Write the new Name:")!;
-
-    try {
-      fs.renameSync(`${renameFile}`, `${newName}`);
-    }
-    catch (error) {
-      console.error(`Error to rename the file ${newName}`);
-    };
-  }
-  loop() { }
-}
-
-
-
-
-
-
-
-
-
-
-
 class Menu {
 
   public showMenu() {
@@ -137,14 +55,89 @@ class Menu {
         break;
 
     };
+  }
+}
 
+class fileManager {
+  private fileManager: number = 0;
 
+  constructor(fileManager: number) {
+    this.fileManager = fileManager;
+  };
 
+  listPath(path: string) {
+    const cont: string[] = fs.readdirSync(path);
+
+    for (let i = 0; i <= path.length; i++) {
+      let content = cont[i];
+
+      console.log(content)
+    };
 
   }
 
-}
+  exitCli() { process.exit(0); };
 
+  create(num: number) {
+    if (num == 1) {
+      const file: string = prompt("Write the file with the extension:")!;
+
+      try {
+        fs.writeFileSync(`${file}`, " is created with CLI inside this directory!!!");
+      }
+      catch (error) {
+        console.error("Error to create the file");
+      };
+    }
+
+    else if (num == 2) {
+      const nameFolder: string = prompt("Write the name of folder:")!;
+
+      try {
+        fs.mkdirSync(`${nameFolder}`);
+      }
+      catch (error) {
+        console.error(`Error to create the folder ${nameFolder}`);
+      };
+    };
+    this.loop();
+  }
+
+  delete() {
+    const pathDelete: string = prompt("Write the path to Delete the file:")!;
+
+    try {
+      fs.rmSync(`${pathDelete}`);
+    }
+    catch (error) {
+      console.error(`Error to Delete the file`);
+    };
+
+    this.loop();
+  }
+
+  rename() {
+    const renameFile: string = prompt("Enter the path of the file for rename:")!;
+
+    const newName: string = prompt("Write the new Name:")!;
+
+    try {
+      fs.renameSync(`${renameFile}`, `${newName}`);
+    }
+    catch (error) {
+      console.error(`Error to rename the file ${newName}`);
+    };
+
+    this.loop();
+  }
+  loop() {
+    const answer: string = prompt("You want to return to the Menu?(y/n)")!;
+
+    if (answer == "y") {
+      cli_menu.showMenu();
+    }
+  }
+}
 
 const cli_menu: Menu = new Menu();
 cli_menu.showMenu();
